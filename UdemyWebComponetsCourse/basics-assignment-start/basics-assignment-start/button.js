4f78a3ddb8c2decc75e8a2e1ef1d120d6e3a474f
Jmar33 <josimarferreira922@yahoo.com.br>
2024-05-15 20:54:15 -0300

    Cria um componente botão

--- UdemyWebComponetsCourse/basics-assignment-start/basics-assignment-start/button.js
+++ UdemyWebComponetsCourse/basics-assignment-start/basics-assignment-start/button.js
@@ -1,5 +1,19 @@
-export class ButtonComponent extends HTMLButtonElement{
-
+class ButtonComponent extends HTMLButtonElement{
+  connectedCallback(){
+    this.addEventListener('click', () => {
+      const infoEl = document.querySelector('p');
+      let isHidden = infoEl.style.display === 'none';
+      if (isHidden) {
+        infoEl.style.display = 'block';
+        this.textContent = 'Hide';
+        isHidden = false;
+      } else {
+        infoEl.style.display = 'none';
+        this.textContent = 'Show';
+        isHidden = true;
+      }
+    });
+  }
 }
 
-customElements.define('uc-button', ButtonComponent)
+customElements.define('uc-button', ButtonComponent, {extends: 'button'})
