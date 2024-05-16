26928106ebeef1a950f0721788c74f46e6b548ff
Jmar33 <josimarferreira922@yahoo.com.br>
2024-05-16 15:00:22 -0300

    [feature] Adiciona tabs ao side-drawer

--- UdemyWebComponetsCourse/stencil/web-components-stencil/src/components/side-drawer/side-drawer.tsx
+++ UdemyWebComponetsCourse/stencil/web-components-stencil/src/components/side-drawer/side-drawer.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from "@stencil/core/";
+import { Component, Prop, State } from "@stencil/core/";
 import { h } from "@stencil/core";
 
 
@@ -9,9 +9,38 @@ import { h } from "@stencil/core";
 })
 export class SideDrawer {
   @Prop({reflect: true}) title: string = "Side Drawer"
-  @Prop({reflect: true}) open: boolean
+  @Prop({reflect: true, mutable: true}) open: boolean // Por padrão as propriedades só podem receber o valor vindo do template HTML elas são unidirecionais
+                                                      // para alterar esse comportamento devemos trocar o valor do mutable para true
+
+  @State() showContactInfo: boolean //O decorator Sate é usado quando queremos monitorar uma propriedade que terá seus valores alterados internamente, dentro do
+                                    //próprio componente
+
+  onCloseDrawer(){
+    console.log('Closing...')
+    this.open = false;
+  }
+
+  onContentChange(content: string){
+    this.showContactInfo = content === 'contact'
+  }
 
   render() {
+    let mainContent = <slot/>
+    if(this.showContactInfo){
+      mainContent= (
+        <div id="contact-information">
+          <h2>Contact Information</h2>
+          <p>You can reach us via phone or email.</p>
+          <ul>
+            <li>Phone: 43439088812</li>
+            <li>
+              E-mail:
+              <a href="mailto:something@something.com">something@something.com</a>
+            </li>
+          </ul>
+        </div>
+      )
+    }
     // let content = null
     // if(this.open){
     //   content = (
@@ -26,9 +55,16 @@ export class SideDrawer {
     // return content
     return (
       <aside>
-        <header><h1>{this.title}</h1></header>
+        <header>
+          <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+        </header>
         <menu>
-          <slot/>
+          <section id="tabs">
+            <button class={!this.showContactInfo ? 'active' : ''} onClick={this.onContentChange.bind(this, 'navigation')}>Navigation</button>
+            <button class={this.showContactInfo ? 'active' : ''} onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
+          </section>
+          {mainContent}
         </menu> 
       </aside> 
     )
