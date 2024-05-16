9701499bfddb5dce8faee5fe05db98df052e2cc6
Jmar33 <josimarferreira922@yahoo.com.br>
2024-05-16 12:42:02 -0300

    [feature] Cria botão para abrir o menu

--- UdemyWebComponetsCourse/stencil/web-components-stencil/src/components/side-drawer/side-drawer.tsx
+++ UdemyWebComponetsCourse/stencil/web-components-stencil/src/components/side-drawer/side-drawer.tsx
@@ -9,15 +9,28 @@ import { h } from "@stencil/core";
 })
 export class SideDrawer {
   @Prop({reflect: true}) title: string = "Side Drawer"
+  @Prop({reflect: true}) open: boolean
 
   render() {
+    // let content = null
+    // if(this.open){
+    //   content = (
+    //     <aside>
+    //       <header><h1>{this.title}</h1></header>
+    //       <menu>
+    //         <slot/>
+    //       </menu> 
+    //     </aside> 
+    //   )
+    // }
+    // return content
     return (
-    <aside>
-      <header><h1>{this.title}</h1></header>
-      <menu>
-        <slot/>
-      </menu> 
-    </aside> 
-  )
+      <aside>
+        <header><h1>{this.title}</h1></header>
+        <menu>
+          <slot/>
+        </menu> 
+      </aside> 
+    )
   }
 }
