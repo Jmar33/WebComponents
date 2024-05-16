ad907317c425d0f266fc3bab3a1b2944c369fb76
Jmar33 <josimarferreira922@yahoo.com.br>
2024-05-16 07:07:35 -0300

    Refatora componente tooltip

--- UdemyWebComponetsCourse/my-first-component/tooltip.js
+++ UdemyWebComponetsCourse/my-first-component/tooltip.js
@@ -2,17 +2,24 @@
 class Tooltip extends HTMLElement{
   constructor(){
     super()
-    this._tooltipContainer
+    this._tooltipIcon
+    this._tooltipVisible = false;
     this._tooltipText = 'Some dummy tooltip text.'
     this.attachShadow({mode: 'open'}) //Associa um componente ao shadow root assim, ele passa a ser desacoplado do light DOM, não interferindo ou sofrendo interferência 
                                       //do restante dos componentes
     this.shadowRoot.innerHTML = `
       <style>
         div {
+          font-weight: normal;
           background-color: black;
           color: white;
           position: absolute;
+          top: 1.5rem;
+          left: 0.75rem;
           z-index: 10;
+          padding: 0.15rem;
+          border-radius: 3px;
+          box-shadow: 1px 1px 6px (0,0,0,0.26);
         }
 
         .highlight{
@@ -26,6 +33,7 @@ class Tooltip extends HTMLElement{
 
         :host(.important){
           background-color: var(--color-primary, #ccc);
+          padding: .15rem;
         }
 
         :host-context(p){
@@ -35,13 +43,13 @@ class Tooltip extends HTMLElement{
         .icon{
           background-color: black;
           color: white;
-          padding: 0.15rem 0.15rem;
+          padding: 0.15rem 0.5rem;
           text-align: center;
           border-radius: 50%;
         }
       </style>
       <slot>Some default </slot>
-      <span class="icon"> (?)</span> 
+      <span class="icon">?</span> 
     `
   }
 
@@ -50,21 +58,56 @@ class Tooltip extends HTMLElement{
     if(this.hasAttribute('text')){
       this._tooltipText = this.getAttribute('text')
     }
-    const tooltipIcon = this.shadowRoot.querySelector('span')
+    this._tooltipIcon = this.shadowRoot.querySelector('span')
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this))
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this))
-    this.shadowRoot.appendChild(tooltipIcon)
     this.style.position = 'relative'
   }
 
+  //Essa callback é executada quando alteramos o valor de alguma propriedade
+  attributeChangedCallback(name, oldValue, newValue){
+    console.log(name, oldValue, newValue);
+    if(oldValue === newValue){
+      return;
+    }
+    if(name === 'text'){
+      this._tooltipText = newValue;
+    }
+
+  }
+
+  // Por padrão o JS não monitora nenhuma propriedade, caso estejamos interessados na mudança de valor de alguma propriedade
+  // devemos explicitar através de um 'get'
+  static get observedAttributes() {
+    return ['text'] 
+  }
+
+  disconnectedCallback(){
+    this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip)
+    this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip)
+  }
+
+  _render(){
+    let tooltipContainer = this.shadowRoot.querySelector('div')
+    if(this._tooltipVisible){
+      tooltipContainer = document.createElement('div')
+      tooltipContainer.textContent = this._tooltipText
+      this.shadowRoot.appendChild(tooltipContainer)
+    }else{
+      if(tooltipContainer){
+        this.shadowRoot.removeChild(tooltipContainer)
+      }
+    }
+  }
+
   _showTooltip(){
-    this._tooltipContainer = document.createElement('div')
-    this._tooltipContainer.textContent = this._tooltipText
-    this.shadowRoot.appendChild(this._tooltipContainer)
+    this._tooltipVisible = true
+    this._render()
   }
 
   _hideTooltip(){
-    this.shadowRoot.removeChild(this._tooltipContainer)
+    this._tooltipVisible = false
+    this._render()
   }
 }
 
